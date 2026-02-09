4df413a8a080e18dcb437e059ca550564c806413
shimbaa <shim9597@gmail.com>
2026-02-09 14:51:12 +0900

    refactor: batch, 정산 도메인 관련 코드, 설정 삭제

--- src/main/java/com/profect/tickle/domain/settlement/repository/SettlementDetailRepository.java
+++
@@ -1,7 +0,0 @@
-package com.profect.tickle.domain.settlement.repository;
-
-import com.profect.tickle.domain.settlement.entity.SettlementDetail;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SettlementDetailRepository extends JpaRepository<SettlementDetail, Long> {
-}
